11508b2b8e01137a2b487e6813d3bff31fbe2482
caoliqingstudio <1714153578@qq.com>
2018-07-26 11:17:25 +0800

    ??

--- trace/traceReport.py
+++ trace/traceReport.py
@@ -1,12 +1,21 @@
 from jinja2 import  Template
 import json
 import time
+from urllib import request
 
 infor = {"id":"id","start_time":15645645,"finish_time":464654,"tor_address":"asd4f54.onio",
-         "site_name":"site_name","ip_address":"127.0.0.1","country":"country",
+         "site_name":"site_name","ip_address":"127.0.0.1",
          "service_type":"bbs","client_ip":"ip who find","path":["relay ip 1","relay ip 2"],
          "clients":["client1","client2","client3"],"stream_id":"stream_id","circ_id":["1","2","3","4"],
          "details":"details"}
+
+def getCountry(ip):
+    url = "http://ip.taobao.com/service/getIpInfo.php?ip="+str(ip)
+    response = request.urlopen(ip)
+    infor=response.read()
+    infor=infor.decode("utf8")
+    infor = json.loads(infor)
+    return infor["country"]+infor["region"]
 
 class traceReport:
     content = ""
@@ -19,6 +28,7 @@ class traceReport:
     def trace2md(self,body):
         infor = json.loads(body)
         #infor = body
+        infor["country"]=getCountry(infor["ip_address"])
         infor["start_time"]=time.asctime(time.localtime(float(infor["start_time"])))
         infor["finish_time"]=time.asctime(time.localtime(float(infor["finish_time"])))
         outfileInfor = self.template.render(**infor)
